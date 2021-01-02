3f132bf158ef0cbcb8d2d062b51ee0615757589b
glaucia86 <glaucia_lemos86@hotmail.com>
2021-01-02 05:20:21 -0300

    Desenvolvimento da rota 'login' e alterações significativas no arquivo 'user.mode.js'

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -16,18 +16,32 @@ exports.registerNewUser = async (req, res) => {
     let isUser = await User.find({ email: req.body.email });
     console.log(isUser);
     if (isUser.length >= 1) {
-      return res.status(409).json({ message: 'Sorry! This email is already registered!' });
+      return res.status(409).json({ message: 'Atenção!  Este e-mail já possui registro na Base de Dados!' });
     }
 
     const newUser = new User(req.body);
     const user = await newUser.save();
     const token = await newUser.generateAuthToken(); // ==> Aqui chamaremos o método que criamos no model
-    res.status(201).json({ message: 'User created successfully!', user, token });
+    res.status(201).json({ message: 'Usuário(a) criado(a) com sucesso!', user, token });
   } catch (err) {
     res.status(400).json({ err: err });
   }
 };
 
-exports.loginUser = async (req, res) => {};
+// ==> Método responsável por realizar um novo login 'User':
+exports.loginUser = async (req, res) => {
+  try {
+    const email = req.body.email;
+    const password = req.body.password;
+    const user = await User.findByCredentials(email, password);
+    if (!user) {
+      return res.status(401).json({ error: 'Erro ao Logar! Verifique as suas credenciais de autenticação!' });
+    }
+    const token = await user.generateAuthToken();
+    res.status(201).json({ message: 'Usuário(a) logado com sucesso!', user, token });
+  } catch (err) {
+    res.status(400).json({ err: err });
+  }
+};
 
 exports.returnUserProfile = async (req, res) => {};
--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -16,7 +16,7 @@ const userSchema = new Schema({
   email: { type: String, maxlength: 30, required: true },
   password: { type: String, required: true },
   tokens: [
-    { 
+    {
       token: { type: String, required: true }
     }
   ]
@@ -26,9 +26,9 @@ const userSchema = new Schema({
 });
 
 // ==> Esse método irá fazer o 'hash' da senha antes de salvar o modelo da classe 'User'
-userSchema.pre('save', async function(next) {
-  const user = this;  
-    if (user.isModified('password')) {
+userSchema.pre("save", async function(next) {
+  const user = this;
+  if (user.isModified("password")) {
     user.password = await bcrypt.hash(user.password, 8);
   }
   next();
@@ -40,23 +40,24 @@ userSchema.methods.generateAuthToken = async function() {
   const token = jwt.sign({ _id: user._id, name: user.name, email: user.email }, "secret");
   user.tokens = user.tokens.concat({ token });
   await user.save();
-
   return token;
 };
 
 // ==> Esse método irá fazer uma pesquisa por um 'user' por 'email' e 'password'
 userSchema.statics.findByCredentials = async (email, password) => {
   const user = await User.findOne({ email });
+  console.log(user);
   if (!user) {
-    throw new Error({ error: 'Invalid Login details.' });
+    throw new Error({ error: 'Login inválido!' });
   }
   const isPasswordMatch = await bcrypt.compare(password, user.password);
   if (!isPasswordMatch) {
-    throw new Error({ error: "Invalid login details." });
+    throw new Error({ error: 'Login inválido!' });
   }
-
   return user;
 };
 
-module.exports = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
 

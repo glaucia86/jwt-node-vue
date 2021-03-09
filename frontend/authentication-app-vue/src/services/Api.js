5f213fcc43b09ce7f529e5d86becf9ee463b6472
glaucia86 <glaucia_lemos86@hotmail.com>
2021-03-09 15:19:39 -0300

    Fix-typos no arquivo 'Api.js''

--- frontend/authentication-app-vue/src/services/Api.js
+++ frontend/authentication-app-vue/src/services/Api.js
@@ -10,5 +10,5 @@ import axios from 'axios';
 
 export default () => axios.create({
   // 'baseURL' do Back-End -> fará a comunicação do Front com o Back
-  baseURL: 'http://localhost:3000/api/v1'
+  baseURL: 'http://localhost:3000/api/v1',
 });

ab66825b890946e7b69f0c904069c0d6c59e0f83
glaucia86 <glaucia_lemos86@hotmail.com>
2021-04-24 01:41:55 -0300

    Criando a tela de Register/Cadastro.

--- frontend/authentication-app-vue/src/components/auth-components/register/Register.js
+++ frontend/authentication-app-vue/src/components/auth-components/register/Register.js
@@ -1,6 +1,6 @@
 /**
  * file: src/components/auth-components/register/Register.js
- * data: 09/11/2020
+ * data: 11/03/2021
  * description: arquivo responsável pela lógica do componente
  *  'Register.vue'
  * author: Glaucia Lemos <twitter: @glaucia_lemos86>
@@ -10,7 +10,18 @@ export default {
   name: 'RegisterComponent',
   data() {
     return {
-
+      registerForm: {
+        name: '',
+        email: '',
+        password: '',
+      },
     };
+  },
+  methods: {
+    registerSubmitUserForm() {},
+
+    async submitRegisterUser() {
+
+    },
   },
 };
--- frontend/authentication-app-vue/src/components/auth-components/register/RegisterComponent.vue
+++ frontend/authentication-app-vue/src/components/auth-components/register/RegisterComponent.vue
@@ -1,6 +1,57 @@
 <template>
-  <div class="row justify-content-center">
-    <h1>Register Page</h1>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6 offset-lg-3 col-sm-10 offset-sm-1">
+        <form
+          class="text-center border border-primary p-5"
+          style="margin-top:70px;height:auto;padding-top:100px !important;"
+          v-on:submit.prevent="registerSubmitUserForm()"
+        >
+        <!--INÍCIO BLOCO: Nome-->
+        <input
+          type="text"
+          id="name"
+          name="name"
+          class="form-control mb-5"
+          placeholder="Inclua seu Nome"
+          v-model="registerForm.name"
+        />
+        <!--FIM BLOCO: Nome-->
+
+        <!--INÍCIO BLOCO: E-mail-->
+        <input
+          type="text"
+          id="email"
+          name="email"
+          class="form-control mb-5"
+          placeholder="Inclua seu E-mail"
+          v-model="registerForm.email"
+        />
+        <!--FIM BLOCO: E-mail-->
+
+        <!--INÍCIO BLOCO: Password-->
+        <input
+          type="text"
+          id="password"
+          name="password"
+          class="form-control mb-5"
+          placeholder="Inclua a Senha"
+          v-model="registerForm.password"
+        />
+        <!--FIM BLOCO: Password-->
+        <p class="center">
+          Já possui um login cadastrado?<router-link to="/"> Faça o Login Aqui</router-link>
+        </p>
+        <!--INÍCIO BLOCO: Botão-->
+        <center>
+          <button @click="submitRegisterUser" class="btn btn-primary btn-block w-75 my-4">
+            Cadastrar
+          </button>
+        </center>
+        <!--FIM BLOCO: Botão-->
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 

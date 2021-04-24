000a4fca3e883c2a33e023a138f73e0357ac378d
glaucia86 <glaucia_lemos86@hotmail.com>
2021-04-24 02:15:51 -0300

    Desenvolvimento da lógica do NProgress

--- frontend/authentication-app-vue/src/main.js
+++ frontend/authentication-app-vue/src/main.js
@@ -4,6 +4,7 @@ import VueSweetalert2 from 'vue-sweetalert2';
 import App from './App.vue';
 import router from './router';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/nprogress/nprogress.css';
 
 Vue.config.productionTip = false;
 
--- frontend/authentication-app-vue/src/router/index.js
+++ frontend/authentication-app-vue/src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import NProgress from 'nprogress';
 
 Vue.use(VueRouter);
 
@@ -25,6 +26,21 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes,
+});
+
+// Lógica inerente ao NProgress
+router.beforeResolve((to, from, next) => {
+  // Se caso não for uma página inicial de carregamento
+  if (to.name) {
+    // Quando houver carregamento de uma página inicial, então usar o NProgress:
+    NProgress.start();
+  }
+  next();
+});
+
+router.afterEach((to, from) => {
+  // Completando a animação da rota no NProgress
+  NProgress.done();
 });
 
 export default router;

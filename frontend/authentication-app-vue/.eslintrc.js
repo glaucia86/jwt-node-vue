1231bd3032572b21ab40238b09aa79beb131df5f
glaucia86 <glaucia_lemos86@hotmail.com>
2021-05-12 13:26:24 -0300

    alterando as regras de linter no arquivo '.eslintrc.js'

--- frontend/authentication-app-vue/.eslintrc.js
+++ frontend/authentication-app-vue/.eslintrc.js
@@ -16,6 +16,8 @@ module.exports = {
     'no-unresolved': 'off',
     'no-empty-function': 'off',
     'no-unused-vars': 'off',
+    'no-alert': 'off',
+    'prefer-template': 'off',
     'no-unused-expressions': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',

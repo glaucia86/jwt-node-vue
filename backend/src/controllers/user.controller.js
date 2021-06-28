56b3134fd3887e48fe7dae34625436320826342b
glaucia86 <glaucia_lemos86@hotmail.com>
2021-06-27 22:09:46 -0300

    mudanças significativas no arquivo .eslintrc

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -5,7 +5,7 @@
  * Author Glaucia Lemos <Twitter: @glaucia_lemos86>
  */
 
-const User = require('../models/user.model');
+const User = require("../models/user.model");
 
 // ==> Usando conceito de Async e Await
 
@@ -16,13 +16,17 @@ exports.registerNewUser = async (req, res) => {
     const isUser = await User.find({ email: req.body.email });
     console.log(isUser);
     if (isUser.length >= 1) {
-      return res.status(409).json({ message: 'Atenção! Este e-mail já possui registro!' });
+      return res
+        .status(409)
+        .json({ message: "Atenção! Este e-mail já possui registro!" });
     }
 
     const newUser = new User(req.body);
     const user = await newUser.save();
     const token = await newUser.generateAuthToken(); // ==> Aqui chamaremos o método que criamos no model
-    return res.status(201).json({ message: 'Usuário(a) criado(a) com sucesso!', user, token });
+    return res
+      .status(201)
+      .json({ message: "Usuário(a) criado(a) com sucesso!", user, token });
   } catch (err) {
     return res.status(400).json({ err });
   }
@@ -35,10 +39,14 @@ exports.loginUser = async (req, res) => {
     const { password } = req.body;
     const user = await User.findByCredentials(email, password);
     if (!user) {
-      return res.status(401).json({ error: 'Erro ao Logar! Verifique as suas credenciais de autenticação!' });
+      return res.status(401).json({
+        error: "Erro ao Logar! Verifique as suas credenciais de autenticação!",
+      });
     }
     const token = await user.generateAuthToken();
-    return res.status(201).json({ message: 'Usuário(a) logado com sucesso!', user, token });
+    return res
+      .status(201)
+      .json({ message: "Usuário(a) logado com sucesso!", user, token });
   } catch (err) {
     return res.status(400).json({ err });
   }

dc161f54a591cfc9d663ea48df1b56d184d51fdf
glaucia86 <glaucia_lemos86@hotmail.com>
2021-07-24 00:23:11 -0300

    removendo pacotes desnecessários.

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,28 +5,27 @@
  * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
  */
 
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const mongooseConnection = require('./config/mongooseConnection.config');
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const mongooseConnection = require("./config/mongooseConnection.config");
 
 const app = express();
 
 // ==> Rotas da API:
-const index = require('./routes/index');
-const userRoutes = require('./routes/user.routes');
+const index = require("./routes/index");
+const userRoutes = require("./routes/user.routes");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.json({ type: 'application/vnd.api+json' }));
-app.use(morgan('dev'));
+app.use(express.json({ type: "application/vnd.api+json" }));
+app.use(morgan("dev"));
 app.use(cors());
 
 // ==> Retornando a conexão via mongoose via external file usando 'app.set()'
-app.set('mongoose connection', mongooseConnection);
+app.set("mongoose connection", mongooseConnection);
 
 app.use(index);
-app.use('/api/v1/', userRoutes);
+app.use("/api/v1/", userRoutes);
 
 module.exports = app;
